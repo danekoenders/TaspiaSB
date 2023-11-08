0f7f6d492f29145e6005d80475a80696212f54e6
Dane Koenders <dane.koenders@gmail.com>
2023-11-08 12:24:43 +0100

    Testing

--- src/main/java/com/taspia/taspiasb/TaspiaSB.java
+++ src/main/java/com/taspia/taspiasb/TaspiaSB.java
@@ -1,17 +1,46 @@
 package com.taspia.taspiasb;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TaspiaSB extends JavaPlugin {
+public class TaspiaSB extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            /*
+             * We register the EventListener here, when PlaceholderAPI is installed.
+             * Since all events are in the main class (this class), we simply use "this"
+             */
+            Bukkit.getPluginManager().registerEvents(this, this);
+        } else {
+            /*
+             * We inform about the fact that PlaceholderAPI isn't installed and then
+             * disable this plugin to prevent issues.
+             */
+            getLogger().warning("Could not find PlaceholderAPI! This plugin is required.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(PlayerJoinEvent event) {
+        String joinText = "%player_name% &ajoined the server! They are rank &f%vault_rank%";
+
+        /*
+         * We parse the placeholders using "setPlaceholders"
+         * This would turn %vault_rank% into the name of the Group, that the
+         * joining player has.
+         */
+        joinText = PlaceholderAPI.setPlaceholders(event.getPlayer(), joinText);
+
+        event.setJoinMessage(joinText);
     }
 }
